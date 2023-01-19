4ab46751b1e70c0fa7930176559358c75efb4a9c
Wellington Oliveira <woliveira@ipt.br>
2023-01-19 16:36:27 -0300

    first version Ubuntu 22.04

---
+++ modules/Ubuntu2204/main.tf
@@ -0,0 +1,75 @@
+provider "vsphere" {
+  user                = var.vsphere_user
+  password            = var.vsphere_password
+  vsphere_server      = var.vsphere_server
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+##### Data sources
+data "vsphere_datacenter" "IPT" {
+  name                 = var.vsphere_datacenter
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name                 = var.vsphere_cluster
+  datacenter_id        = data.vsphere_datacenter.IPT.id
+}
+
+data "vsphere_network" "network" {
+  name                 = var.vsphere_network
+  datacenter_id        = data.vsphere_datacenter.IPT.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name                 = var.vsphere_template
+  datacenter_id        = data.vsphere_datacenter.IPT.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name                 = "${var.datastore}"
+  datacenter_id        = "${data.vsphere_datacenter.IPT.id}"
+}
+
+
+resource "vsphere_virtual_machine" "ubuntu2204" {
+  
+  name                       = "${var.vm_name}"
+  num_cpus                   = "${var.vm_cpus}"
+  memory                     = var.vm_memory
+  folder                     = "${var.vsphere_folder}"
+  resource_pool_id           = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id               = data.vsphere_datastore.datastore.id
+  guest_id                   = data.vsphere_virtual_machine.template.guest_id
+  
+  
+  disk {
+    label                    = "disk0"
+    size                     = data.vsphere_virtual_machine.template.disks.0.size
+    thin_provisioned         = false
+  }
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  clone {
+    template_uuid            = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name            = "${var.vm_name}"
+        domain               = "${var.domain}"
+      }
+      network_interface {
+        ipv4_address         = "${var.ip_address}"
+        ipv4_netmask         = "${var.cidr}"
+        
+      }
+      dns_server_list       = "${var.dns_servers_list}"
+      dns_suffix_list        = [ "${var.domain}" ]
+      ipv4_gateway           = "${var.vsphere_network_gateway}"
+    }
+  }
+  
+}
---
+++ modules/Ubuntu2204/variables.tf
@@ -0,0 +1,128 @@
+#variables.tf
+
+variable "vsphere_server" {
+  description = "vCenter server FQDN or IP"
+  
+}
+
+variable "vsphere_user" {
+  description = "vSphere username to use to connect to the environment"
+  sensitive   = true
+}
+
+variable "vsphere_password" {
+  description = "vSphere password"
+  sensitive   = true
+}
+
+variable "ubuntu_user" {
+  description = "Username to use to connect ssh and run provisioning shell "
+  type        = string
+  sensitive   = true
+}
+
+variable "ubuntu_password" {
+  description = "Password to use to connect ssh and run provisioning shell "
+  type        = string
+  sensitive   = true
+}
+
+
+#VM Location
+
+variable "vsphere_datacenter" {
+  description = "vSphere datacenter"
+  default     = "IPT"
+  type        = string
+}
+
+variable "vsphere_cluster" {
+  description = "vSphere cluster"
+  type        = string
+}
+
+variable "vsphere_folder" {
+  description = "vSphere folder where to create the VM"
+  type        = string
+}
+
+variable "datastore" {
+  description = "datastores available for storage allocation. By name ex: datastore1,datastore2"
+  type        = string
+}
+
+
+
+
+# Virtual Machine Settings
+
+# for vault vm
+
+variable "vm_cpus" {
+  description = "Number of CPUs"  
+  type        = number
+  default     = 4
+}
+
+variable "vm_memory" {
+  description = "Memory in MB"
+  type        = number
+  default      = 8196
+
+}
+
+variable "vm_disk_size" {
+  description = "Disk size in GB"
+  type        = number
+  default     = 100
+}
+
+variable "vsphere_network" {
+  description = "vSphere network"
+  type        = string
+  
+}
+
+variable "vsphere_template" {
+  description = "vSphere template which will be used to create the VM"
+  type        = string
+  default     = "UbuntuServer2204packer"
+}
+
+# VM customization
+
+variable "vm_name" {
+  description = "New VM's Name"
+  type        = string
+  
+}
+
+variable "domain" {
+  description = "Domain name"
+  type        = string
+  
+}
+
+variable "dns_servers_list" {
+  description = "DNS server list format ['xxx.xxx.xxx.xxx',xx.xx.xx.xx' ]"
+  type        = list
+  
+}
+
+variable "ip_address" {
+  description = "VM Ipv4 Address"
+  type        = string
+  
+}
+
+variable "cidr" {
+  description = "CIDR for netmask formation. two digits ex. 10.11.39.0/XX  XX=16"
+  type        = number
+  default = 16
+}
+
+variable "vsphere_network_gateway" {
+  description = "vSphere network gateway"
+  type        = string
+  
+}
